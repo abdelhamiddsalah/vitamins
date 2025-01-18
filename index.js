0f9c876202aab3241d11b1aebef64f17f7eac669
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-01-18 18:18:20 +0200

    add

--- index.js
+++ index.js
@@ -12,16 +12,13 @@ connectDB();
 // Middleware
 const app = express();
 
-app.set('view engine', 'ejs');
 
-// تحديد مسار القوالب (إذا كانت الملفات داخل مجلد محدد مثل "views")
-app.set('views', './views');
 
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
-
+app.set('view engine', 'ejs');
 // Routes
 const productsRoute = require('./routes/products-route');
 const userRoute = require('./routes/user-route');
